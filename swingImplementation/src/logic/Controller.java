846b8b12449adbed836c1092345e3ec27673f6c0
nicocadq <nmachado.camejo@gmail.com>
2020-08-03 19:25:24 -0300

    Created some logic to have a functional UI

--- swingImplementation/src/logic/Controller.java
+++ swingImplementation/src/logic/Controller.java
@@ -1,5 +1,23 @@
 package logic;
 
+import java.util.ArrayList;
+
 public class Controller {
 
+	private ArrayList<Product> products;
+	
+	public Controller() {
+		this.products = new ArrayList<Product>();
+	}
+	
+	
+	public ArrayList<Product> getProducts(){
+		return this.products;
+	}
+	
+	
+	public void saveProduct(int id, String nombre, String direction, int avilableAmount) {
+		this.products.add(new Product(id, nombre, direction, avilableAmount));
+	}
+	
 }
--- swingImplementation/src/logic/Product.java
+++ swingImplementation/src/logic/Product.java
@@ -1,5 +1,49 @@
 package logic;
 
 public class Product {
+	
+	private int id;
+	private String name;
+	private String direction;
+	private int avilableAmount;
+	
+	public Product(int id, String name, String direction, int avilableAmount) {
+		this.name = name;
+		this.direction = direction;
+		this.avilableAmount = avilableAmount;
+		this.id = id;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+
+	public int getAvilableAmount() {
+		return avilableAmount;
+	}
+
+	public void setAvilableAmount(int avilableAmount) {
+		this.avilableAmount = avilableAmount;
+	}
+	
 }
